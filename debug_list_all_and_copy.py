693d556b265fd535d985e50cf863602c832b266a
xuananh000 <xuananhbka@gmail.com>
2024-06-17 15:52:23 +0700

    List all and copy

--- debug_list_all_and_copy.py
+++ debug_list_all_and_copy.py
@@ -6,20 +6,34 @@ import os
 import glob
 import argparse
 import re
+import shutil
+import numpy as np
+from tqdm import tqdm
 
 # Create argument parser
 parser = argparse.ArgumentParser(description="Debug check folder")
 parser.add_argument("--input_path", type=str, help="Input path")
 parser.add_argument("--output_path", type=str, help="Depth")
+parser.add_argument("--number_copy", type=int, help="Number of copy")
 args = parser.parse_args()
 
 
 # Count all trajectory in a directory
 
-def debug_count_and_copy(input_path, output_path):
+def debug_count_and_copy(input_path, output_path, number_copy):
     count_all = 0
     # List all sub directories in input_path have ".*traj[0-9]" pattern
     paths = glob.glob(f'{input_path}/**/traj[0-9]*', recursive=True)
     print(len(paths))
-    
-debug_count_and_copy(args.input_path, args.output_path)
+    os.makedirs(output_path, exist_ok=True)
+    # Choose randomly number_copy directories to copy
+    paths_copy = np.random.choice(paths, number_copy, replace=False)
+    for path in tqdm(paths_copy):
+        # Split input_path to get the last directory name
+        # Example: input_path = data/demo_8_17/raw/<PATH_TO_TRAJECTORY>/traj0
+        # Split input_path to get <PATH_TO_TRAJECTORY>/traj0
+        path_target = path.replace(input_path, "")
+        shutil.copytree(path, os.path.join(output_path, path_target))
+        
+        
+debug_count_and_copy(args.input_path, args.output_path, number_copy=args.number_copy)
